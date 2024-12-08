4b0c8425759c66730ec1618acc7b6c0e91eaa246
Jon Narine <jon.wolf.narine@gmail.com>
2024-12-08 16:45:12 -0500

    Adds venue migration

---
+++ src/WareHaus/Tardis/migrations/20241208183122-create-venues.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('venues', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address1: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address2: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      zip: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      age_policy: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      photos: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: false
+      },
+      cancellation_policy_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'cancellation_policies',
+          key: 'policy_id'
+        }
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('venues')
+  }
+};
